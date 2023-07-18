d600612b4988af49fe05b19d0aa650587e226e97
Srvand <anand.elango@gmail.com>
2023-07-18 17:11:24 +0530

    Add files via upload

--- LangChain_V2.py
+++ LangChain_V2.py
@@ -1,17 +1,20 @@
+from dotenv import load_dotenv
 import streamlit as st
+# from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS,Chroma
+from langchain.chains import RetrievalQA,RetrievalQAWithSourcesChain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.llms import OpenAI
+from langchain.document_loaders import TextLoader,PyPDFLoader
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
-from PyPDF2 import PdfReader
-from langchain.chains import RetrievalQA
 import os
-from langchain.chat_models import ChatOpenAI
 import tempfile
 from langchain.llms import OpenAI
+from PyPDF2 import PdfReader
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
@@ -19,6 +22,10 @@ from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+    # # print('Hello')
+st.set_page_config(page_title="Search PDF for answer")
+
 
 def add_document(file):  
     if file.type == 'application/pdf':
@@ -29,7 +36,8 @@ def add_document(file):
             loader = PyPDFLoader(temp_file_path)
             pages = loader.load()
             # st.write(pages)
-        return pages   
+        return pages
+          
     else:
         st.warning(f"{file.name} is not a supported file format")
 
@@ -39,7 +47,12 @@ def get_file_name_with_extension(path):
     file_name_with_extension = file_name 
     return file_name_with_extension
 
-st.set_page_config(page_title='Contextualized Search for Document Archive using Langchain',layout="wide")
+def extract_source_and_page(document):
+    source = document.metadata["source"]
+    source=get_file_name_with_extension(source)
+    page = document.metadata["page"]
+    return source, page
+
 st.write("""
     <style>
         footer {visibility: hidden;}
@@ -47,26 +60,27 @@ st.write("""
             font-family: Arial, sans-serif;
         }
     </style>
-""", unsafe_allow_html=True)
-st.write(f"<h1 style='font-size: 36px; color: #00555e; font-family: Arial;text-align: center;'>Contextualized Search for Document Archive using LangChain 🦜️🔗</h1>", unsafe_allow_html=True)
+""", unsafe_allow_html=True)#Update V2
+# st.title("Contextualized Search for Document Archive")#Update V2
+st.write(f"<h1 style='font-size: 36px; color: #00555e; font-family: Arial;text-align: center;'>Contextualized Search for Document Archive using LangChain</h1>", unsafe_allow_html=True)
 # create file uploader
-uploaded_files = st.file_uploader("Upload Files", accept_multiple_files=True,type=['pdf'])    
+uploaded_files = st.file_uploader("Upload Files", accept_multiple_files=True)    
 
 if uploaded_files:
     doc_list=[]
     for file in uploaded_files:
         pages=add_document(file) 
-    
+
         text_splitter = CharacterTextSplitter(
         separator = "\n",
         chunk_size = 1000,
         chunk_overlap  = 200, #striding over the text
         length_function = len)
         texts = text_splitter.split_documents(pages)
+        # st.write(texts)
 
         for text in texts:
-            doc_list.append(text)
-        
+            doc_list.append(text)      
 
     embeddings = OpenAIEmbeddings()
     kbase=FAISS.from_documents(doc_list, embeddings)
@@ -76,12 +90,20 @@ if uploaded_files:
             Context: {context}
             Question: {question}
             Answer:"""
+    
+    prompt_template1 = """Given the context and the given question,provide a comprehensive answer in detail from the relevant information presented in the context.
+           If you don't know the answer, just reply with 'No relevant context in documents', don't try to make up an answer.
+            Context: {context}
+            Question: {question}
+            Answer:"""
+
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["context","question"])
+    PROMPT1 = PromptTemplate(template=prompt_template1, input_variables=["context","question"])
 
     st.write(f"<p style='font-size: 16px; color: red;font-family: Arial;'>Ask a question:</p>",unsafe_allow_html=True)
     query = st.text_input(label='Ask a question:',label_visibility="collapsed")
 
-    retriever = kbase.as_retriever(search_type="similarity", search_kwargs={"k":4})
+    retriever = kbase.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
     rqa1 = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4",temperature=0),
                                     chain_type="stuff",
@@ -90,28 +112,28 @@ if uploaded_files:
                                     "prompt": PromptTemplate(template=prompt_template,input_variables=["context", "question"])},
                                     return_source_documents=True)
 
-    rqa2 = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0),
+    rqa2 = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo-16k",temperature=0),
                                     chain_type="stuff",
                                     retriever=retriever,
                                     chain_type_kwargs={
                                     "prompt": PromptTemplate(template=prompt_template,input_variables=["context", "question"])},
                                     return_source_documents=True)    
-    
-    col1, col2 = st.columns(2,gap="small")
+
+    col1, col2= st.columns(2,gap="small")
     with st.form("Models"):
         with col1:
             model1 = st.checkbox("GPT4")
         with col2:
-            model2 = st.checkbox("GPT3.5-turbo")
+            model2 = st.checkbox("gpt-3.5-turbo-16k")
         submit_button = st.form_submit_button("Search Document",use_container_width=True)
 
     if submit_button: 
+
         if model1:
             result1=rqa1(query)
-            st.write('Results generated using GPT4 model:')
             st.write(result1['result'])
             if ((result1['result'])!='No relevant context in documents.'):
-                # st.write(result1['source_documents'])
+                st.write(result1['source_documents'])
                 file_name=get_file_name_with_extension(result1['source_documents'][0].metadata['source'])
                 st.write('Source: ',file_name)
                 page_num=result1['source_documents'][0].metadata['page']
@@ -121,7 +143,6 @@ if uploaded_files:
 
         if model2:
             result2=rqa2(query)
-            st.write('Results generated using GPT3.5 turbo model:')
             st.write(result2['result'])
             if ((result2['result'])!='No relevant context in documents.'):
                 st.write(result2['source_documents'])
@@ -131,4 +152,5 @@ if uploaded_files:
                 page_num=page_num+1
                 page_num=f"Page#: {page_num:02d}"
                 st.write(page_num)
+
         
